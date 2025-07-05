0aaeff169dd50ff3badb261dc13a282ab7821828
vivek <viveksagar6655@gmail.com>
2025-07-06 00:13:41 +0530

    this is my first commit

---
+++ src/com/demo/Demo.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<10;i++) {
+			
+			System.out.println("Hello vivek : "+i);
+		}
+
+	}
+
+}
